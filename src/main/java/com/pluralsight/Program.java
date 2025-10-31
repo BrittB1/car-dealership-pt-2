9ecd93a6616cab94adf7bb20ff09b485ea96d8b1
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-10-31 14:32:05 -0400

    Initial commit. Added base files of CarDealership. Added superclass Contract and SalesContract class.

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+// only runs main program; this is the 'front door'
+public class Program {
+    public static void main(String[] args) {
+
+        UserInterface ui = new UserInterface();
+        ui.showHomeScreen();
+    }
+}
